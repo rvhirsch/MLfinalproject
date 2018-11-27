5f3f5b51a662552ffa867afe6529e17acdc8f70c
rvhirsch <rhirsch@pugetsound.edu>
2018-11-27 15:39:19 -0700

    added imports

--- mainrunner.py
+++ mainrunner.py
@@ -8,6 +8,13 @@ import getdata as data
 import linearsvc as svc
 import errorcalc as error
 
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Perceptron
+
+import warnings
+warnings.filterwarnings("ignore") # suppress warnings
+
 fulldata = data.get_data("yelpdata.csv")
 # smalldata = fulldata[:100]
 # xTrain, xTest, yTrain, yTest = data.split_data(smalldata)
